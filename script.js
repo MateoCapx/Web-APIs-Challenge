362ae2b2265b0fff00f3da067fc2d610e4d7d026
Mateo <mateom.realtor@gmail.com>
2022-05-09 11:16:13 -0400

    updated the HTML, CSS & Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Targeting the Submit button 
+
+let submitBtn = document.querySelector("#submitBtn")
+submitBtn.addEventListener("click", btnSubmit);
+
+function btnSubmit() {
+    console.log (" function worksss !! ")
+}
